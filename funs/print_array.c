25e6150f818e44afba6d89189d81fd4cbb0d6ed3
srikanthpragada <srikanthpragada@gmail.com>
2024-05-27 17:54:47 +0530

    27-MAY

---
+++ funs/print_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+void print(int a[], int len)
+{
+  int i;
+
+     for(i = 0; i < len ; i ++)
+     {
+         printf("%5d", a[i]);
+     }
+
+}
+
+void main()
+{
+
+  int a [] = {1,3,4,5,6,7,4,3,2,1};
+  int b [] = {1,3,4,5,6};
+
+      print(a, 10);
+      printf("\n");
+      print(b, 5);
+
+}
